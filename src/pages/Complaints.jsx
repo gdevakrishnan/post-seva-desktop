2a7e6861c5435749a6e1ded9b5d7180b73c1cc2d
johnwesly08 <johnwesleywork08@gmail.com>
2024-12-03 11:14:14 +0530

    Complaint page added

--- src/pages/Complaints.jsx
+++ src/pages/Complaints.jsx
@@ -1,19 +1,142 @@
-// a.	It displays the cards of complaints
-// b.	It displays the complaint subject, description
-// c.	If I click this it shows the form field to get the review from the authority
-// d.	Button to submit and resolve or forward button to forward the complaint to the next authority
+import React, { Fragment } from "react";
+import "../static/complaints.css";
+import Navbar from "../components/Navbar";
 
-import React, { Fragment } from 'react'
-import '../static/complaints.css'
-
-function Complaints() {
+export default function Complaints() {
   return (
     <Fragment>
-        <section className="page complaints_page">
-            <h1>Complaints page</h1>
-        </section>
+        <Navbar />
+      <div className="complaints">
+        <h2>Grievance</h2>
+        <div className="complaints_data">
+          <div className="complaints_count">
+            <ul>
+              <li>
+                <p>4</p>
+                <h3>Total Complaints</h3>
+              </li>
+              <li>
+                <p>3</p>
+                <h3>Solved Complaints</h3>
+              </li>
+              <li>
+                <p>2</p>
+                <h3>Pending Complaints</h3>
+              </li>
+            </ul>
+          </div>
+          <div className="search">
+            <div className="bar">
+              <input type="search" name="searchbar" id="searchbar" placeholder="Search COmplaint"/>
+            </div>
+            <div className="filterbtn">
+              <button type="submit">Filter</button>
+            </div>
+          </div>
+        </div>
+        <div className="complaints_content">
+          <div className="box1">
+            <div className="content1">
+              <p>
+                <strong>Complaint No:</strong>20036168
+              </p>
+              <p>
+                <strong>Name :</strong>Kamalesh
+              </p>
+              <p>
+                <strong>Description :</strong>Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Amet esse fugiat voluptas, ullam
+                reprehenderit nemo quo expedita cumque animi facere.
+              </p>
+            </div>
+            <div className="content2">
+              <p>
+                <strong>Complaint Date :</strong>28-11-2024
+              </p>
+              <p>
+                <strong>Status :</strong>Untracked
+              </p>
+              <button type="submit">View more</button>
+            </div>
+          </div>
+
+          <div className="box1">
+            <div className="content1">
+              <p>
+                <strong>Complaint No :</strong>20036168
+              </p>
+              <p>
+                <strong>Name:</strong>Kamalesh
+              </p>
+              <p>
+                <strong>Description  :</strong>Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Amet esse fugiat voluptas, ullam
+                reprehenderit nemo quo expedita cumque animi facere.
+              </p>
+            </div>
+            <div className="content2">
+              <p>
+                <strong>Complaint Date :</strong>28-11-2024
+              </p>
+              <p>
+                <strong>Status :</strong>Untracked
+              </p>
+              <button type="submit">View more</button>
+            </div>
+          </div>
+
+          <div className="box1">
+            <div className="content1">
+              <p>
+                <strong>Complaint No:</strong>20036168
+              </p>
+              <p>
+                <strong>Name :</strong>Kamalesh
+              </p>
+              <p>
+                <strong>Description :</strong>Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Amet esse fugiat voluptas, ullam
+                reprehenderit nemo quo expedita cumque animi facere.
+              </p>
+            </div>
+            <div className="content2">
+              <p>
+                <strong>Complaint Date :</strong>28-11-2024
+              </p>
+              <p>
+                <strong>Status :</strong>Untracked
+              </p>
+              <button type="submit">View more</button>
+            </div>
+          </div>
+
+          <div className="box1">
+            <div className="content1">
+              <p>
+                <strong>Complaint No:</strong>20036168
+              </p>
+              <p>
+                <strong>Name :</strong>Kamalesh
+              </p>
+              <p>
+                <strong>Description :</strong>Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Amet esse fugiat voluptas, ullam
+                reprehenderit nemo quo expedita cumque animi facere.
+              </p>
+            </div>
+            <div className="content2">
+              <p>
+                <strong>Complaint Date :</strong>28-11-2024
+              </p>
+              <p>
+                <strong>Status :</strong>Untracked
+              </p>
+              <button type="submit">View more</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
-  )
+  );
 }
 
-export default Complaints
